bcbda3d2a7ea9cf80b9340cd1919c736eb829ea9
Bound19375 <54005601+Bound19375@users.noreply.github.com>
2023-02-27 10:03:06 +0100

    Add project files.

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks();
+builder.Services.AddHttpForwarder();
+
+//YARP
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+//TLS
+var certificate = new X509Certificate2(builder.Configuration["Kestrel:Certificates:Default:Path"], builder.Configuration["Kestrel:Certificates:Default:Password"]);
+builder.WebHost.UseKestrel(options => {
+    options.Listen(IPAddress.Any, 80, listenOptions => {
+        listenOptions.UseConnectionLogging();
+    });
+
+    options.Listen(IPAddress.Any, 443, listenOptions => {
+        listenOptions.UseHttps(certificate);
+        listenOptions.UseConnectionLogging();
+    });
+});
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health");
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
