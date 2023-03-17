5f933ed2ccea5d2e245422ce986da701793fc7d5
Bound19375 <54005601+Bound19375@users.noreply.github.com>
2023-03-17 16:35:56 +0100

    Discord.Net

--- BoundBot/Program.cs
+++ BoundBot/Program.cs
@@ -11,9 +11,6 @@ namespace BoundBot;
 
 public class DiscordBot
 {
-    static readonly DiscordSocketClient Client = DiscordClient.GetDiscordSocketClient();
-    static readonly CommandService Service = DiscordClient.GetCommandService();
-
     public static Task Main(string[] args) => new DiscordBot().MainAsync();
 
     async Task MainAsync() {
@@ -23,33 +20,40 @@ public class DiscordBot
             .Build();
 
         var service = new ServiceCollection();
+        service.AddSingleton<IConfiguration>(configBuilder);
+        service.AddSingleton(DiscordClient.GetDiscordSocketClient());
+        service.AddSingleton(DiscordClient.GetCommandService());
         service.AddHttpClient("httpClient", httpClient =>
         {
             httpClient.BaseAddress = new Uri(configBuilder["HttpClient:connStr"] ?? string.Empty);
         });
         service.AddScoped<SlashCommandsHandler>();
         service.AddScoped<CommandHandler>();
-        service.AddTransient<SlashCommandsBuilder>(_ => new SlashCommandsBuilder(Client));
+        service.AddScoped<SlashCommandsBuilder>();
         var serviceProvider = service.BuildServiceProvider();
 
-        Client.Log += Log;
+        DiscordSocketClient client = serviceProvider.GetService<DiscordSocketClient>()!;
+        CommandService commandService = serviceProvider.GetRequiredService<CommandService>();
 
-        var cHandler = serviceProvider.GetRequiredService<CommandHandler>();
+        client.Log += Log;
+
+        CommandHandler cHandler = new(client, commandService);
         await cHandler.InstallCommandsAsync();
 
-        var builder = serviceProvider.GetRequiredService<SlashCommandsBuilder>();
-        Client.Ready += builder.Client_Ready;
+        var builder = serviceProvider.GetService<SlashCommandsBuilder>();
+        client.Ready += builder!.Client_Ready;
 
-        var sHandler = serviceProvider.GetRequiredService<SlashCommandsHandler>();
-        Client.SlashCommandExecuted += sHandler.SlashCommandHandler;
+        var sHandler = serviceProvider.GetService<SlashCommandsHandler>();
+        client.SlashCommandExecuted += sHandler.SlashCommandHandler;
 
         // Block this task until the program is closed.
         await Task.Delay(Timeout.Infinite);
     }
 
-//AddScoped is used when you want to create a new instance of a service for each request within the scope.This means that if you request the same service multiple times within the same scope, you'll get the same instance.
-//AddTransient is used when you want to create a new instance of a service every time it is requested.This means that if you request the same service multiple times, you'll get a different instance each time.
-//AddSingleton is used when you want to create a single instance of a service for the lifetime of the application.This means that if you request the same service multiple times, you'll get the same instance each time.
+
+    //AddScoped is used when you want to create a new instance of a service for each request within the scope.This means that if you request the same service multiple times within the same scope, you'll get the same instance.
+    //AddTransient is used when you want to create a new instance of a service every time it is requested.This means that if you request the same service multiple times, you'll get a different instance each time.
+    //AddSingleton is used when you want to create a single instance of a service for the lifetime of the application.This means that if you request the same service multiple times, you'll get the same instance each time.
 
     private Task Log(LogMessage msg)
     {
