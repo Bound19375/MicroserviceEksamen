454676f61f5aacea76ae8eecda4b0fdc5e06bbe5
Bound19375 <54005601+Bound19375@users.noreply.github.com>
2023-04-14 11:33:40 +0200

    State Machine Update

--- DiscordSaga/Saga.cs
+++ DiscordSaga/Saga.cs
@@ -1,76 +1,66 @@
-﻿using System.Data;
-using Auth.Database;
-using Crosscutting.TransactionHandling;
-using MassTransit;
-using Microsoft.Extensions.Logging;
-using DiscordBot.Application.Interface;
+﻿using MassTransit;
 using Crosscutting;
-using Crosscutting.SellixPayload;
 using DiscordSaga.Components.Events;
-using Newtonsoft.Json;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace DiscordSaga
+namespace DiscordSaga;
+
+public class LicenseState : SagaStateMachineInstance, ISagaVersion
 {
-    public class LicenseState : SagaStateMachineInstance, ISagaVersion
+    public Guid CorrelationId { get; set; }
+    public string? Payload { get; set; }
+    public int? Quantity { get; set; }
+    public DateTime Time { get; set; }
+    public WhichSpec? WhichSpec { get; set; }
+    public int CurrentState { get; set; }
+    public int Version { get; set; }
+}
+
+public class LicenseStateMachine : MassTransitStateMachine<LicenseState>
+{
+    public State Granted { get; set; }
+    public State NotificationReady { get; set; }
+    public Event<LicenseGrantEvent> GrantLicense { get; set; }
+    public readonly TaskCompletionSource<bool> GrantLicenseEventProcessed = new();
+    public Event<LicenseNotificationEvent> Notify { get; set; }
+
+
+    public LicenseStateMachine()
     {
-        public Guid CorrelationId { get; set; }
-        public string? Payload { get; set; }
-        public int? Quantity { get; set; }
-        public DateTime Time { get; set; }
-        public WhichSpec? WhichSpec { get; set; }
-        public int CurrentState { get; set; }
-        public int Version { get; set; }
+        Event(() => GrantLicense, x  => x.CorrelateById(context => context.Message.CorrelationId));
+        Event(() => Notify, x => x.CorrelateById(context => context.Message.CorrelationId));
+
+        InstanceState(x => x.CurrentState, Initial, Final, Granted, NotificationReady);
+
+        Initially(
+            When(GrantLicense)
+                .ThenAsync(async context =>
+                {
+                    await GrantLicenseEventProcessed.Task;
+                })
+                .Then(context =>
+                {
+                    context.Saga.CorrelationId = context.Message.CorrelationId;
+                    context.Saga.Payload = context.Message.Payload;
+                    context.Saga.Quantity = context.Message.Quantity;
+                    context.Saga.Time = context.Message.Time;
+                    context.Saga.WhichSpec = context.Message.WhichSpec;
+                })
+                .Produce(context => context.Init<LicenseNotificationEvent>(new
+                {
+                    context.Saga.CorrelationId,
+                    context.Saga.Payload,
+                    context.Saga.Quantity,
+                    context.Saga.Time,
+                    context.Saga.WhichSpec
+                }))
+                .TransitionTo(NotificationReady));
+
+        During(NotificationReady,
+            When(Notify)
+                .Finalize());
+
+        SetCompletedWhenFinalized();
     }
-
-    public class LicenseStateMachine : MassTransitStateMachine<LicenseState>
-    {
-        public State Granted { get; set; }
-        public State NotificationReady { get; set; }
-        public Event<LicenseGrantEvent> GrantLicense { get; set; }
-        public readonly TaskCompletionSource<bool> GrantLicenseEventProcessed = new();
-        public Event<LicenseNotificationEvent> Notify { get; set; }
-
-
-        public LicenseStateMachine()
-        {
-            Event(() => GrantLicense, x  => x.CorrelateById(context => context.Message.CorrelationId));
-            Event(() => Notify, x => x.CorrelateById(context => context.Message.CorrelationId));
-
-            InstanceState(x => x.CurrentState, Initial, Final, Granted, NotificationReady);
-
-            Initially(
-                When(GrantLicense)
-                    .ThenAsync(async context =>
-                    {
-                        await GrantLicenseEventProcessed.Task;
-                    })
-                    .Then(context =>
-                    {
-                        context.Saga.CorrelationId = context.Message.CorrelationId;
-                        context.Saga.Payload = context.Message.Payload;
-                        context.Saga.Quantity = context.Message.Quantity;
-                        context.Saga.Time = context.Message.Time;
-                        context.Saga.WhichSpec = context.Message.WhichSpec;
-                    })
-                    .Produce(context => context.Init<LicenseNotificationEvent>(new
-                    {
-                        context.Saga.CorrelationId,
-                        context.Saga.Payload,
-                        context.Saga.Quantity,
-                        context.Saga.Time,
-                        context.Saga.WhichSpec
-                    }))
-                    .TransitionTo(NotificationReady));
-
-            During(NotificationReady,
-                When(Notify)
-                    .Finalize());
-
-            SetCompletedWhenFinalized();
-        }
-    }
-  
 }
 
 
